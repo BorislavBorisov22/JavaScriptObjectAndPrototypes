350f29b1d4da0acf221e4f40e0a6dff8f098ed28
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-04-08 01:14:21 +0300

    added more prototype exmaples

---
+++ prototypes-and-inheritance/prototype.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function Cat(name, color) {
+    this.name = name;
+    this.color = color;
+}
+
+const fluffy = new Cat('fluffy', 'red');
+
+console.log(Cat.prototype);
+console.log(fluffy.__proto__);
+console.log(Cat.prototype === fluffy.__proto__);
