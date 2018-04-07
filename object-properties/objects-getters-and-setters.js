b4a8ac5f94cfb473198361cb2a6df70f4567eb95
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-04-08 00:57:29 +0300

    added objects getters and setters examples

--- object-properties/objects-getters-and-setters.js
+++ object-properties/objects-getters-and-setters.js
@@ -1,6 +1,25 @@
 'use strict';
 
 const cat = {
-    name: { first: 'fluffy', last: 'fluffy' },
+    name: { first: 'fluffy', last: 'fluff' },
     color: 'red'
 };
+
+Object.defineProperty(cat, 'fullname', {
+    get: function() {
+        return `${this.name.first} ${this.name.last}`;
+    },
+    set: function(value) {
+        const nameParts = value.split(' ');
+        this.name.first = nameParts[0];
+        this.name.last = nameParts[1];
+    }
+});
+
+console.log(cat.fullname);
+
+cat.fullname = 'new name';
+
+console.log(cat.fullname);
+console.log(cat.name.first);
+console.log(cat.name.last);
